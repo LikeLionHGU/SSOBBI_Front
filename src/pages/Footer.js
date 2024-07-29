7a414d30d96a091025c39057e9d40e48354b01c1
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-07-29 18:44:54 +0900

    #41 style: 푸터 디자인 적용

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,18 +1,76 @@
 import styled from "styled-components";
-import { NoCenterHorizontal, Vertical } from "../styles/CommunalStyle";
+import {
+  NoCenterHorizontal,
+  NoCenterVertical,
+  Horizontal,
+} from "../styles/CommunalStyle";
+import LogoImg from "../imgs/Logo.png";
+import { BsInstagram } from "react-icons/bs";
 
+const Title = styled.p`
+  color: white;
+  font-family: "RowdiesBold";
+  font-weight: 700;
+  font-style: normal;
+  font-size: 28px;
+  margin: 0;
+  margin-top: 10px;
+`;
+const Logo = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 15px;
+`;
 const Box = styled.div`
   width: 100%;
   height: 480px;
   margin-top: 100px;
-  background-color: #d3dce7;
+  background-color: black;
+`;
+const Text = styled.p`
+  color: white;
+  font-family: "RowdiesLight";
+  font-style: normal;
+  font-size: 18px;
+  margin: 0;
+  margin-top: 20px;
+  width: 340px;
+`;
+const Insta = styled.p`
+  color: white;
+  font-family: "RowdiesLight";
+  font-style: normal;
+  font-size: 18px;
 `;
 
 export default function Footer() {
   return (
     <Box>
       <NoCenterHorizontal>
-        <Vertical>Footer</Vertical>
+        <NoCenterVertical style={{ marginTop: "110px", marginLeft: "90px" }}>
+          <Horizontal
+            style={{
+              justifyContent: "flex-start",
+              marginTop: "30px",
+            }}
+          >
+            <Logo src={LogoImg} />
+            <Title>SSOBBI</Title>
+          </Horizontal>
+          <Text>
+            감정적 소비를 넘어 합리적 쏘삐를 지향합니다. 대한민국 소비의 지향점,
+            쏘삐
+          </Text>
+          <Horizontal
+            style={{
+              justifyContent: "flex-start",
+              marginTop: "100px",
+            }}
+          >
+            <BsInstagram style={{ color: "white", marginRight: "15px" }} />
+            <Insta>@SSOBBI</Insta>
+          </Horizontal>
+        </NoCenterVertical>
       </NoCenterHorizontal>
     </Box>
   );
