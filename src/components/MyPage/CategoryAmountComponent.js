4c01a22171f00bef52be048310032ced6ddbe83c
sungyu0309 <tjsrbyt@gmail.com>
2024-07-29 16:23:45 +0900

    feat : 목표금액 수정 및 삭제

--- src/components/MyPage/CategoryAmountComponent.js
+++ src/components/MyPage/CategoryAmountComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Vertical, Horizontal } from "../../styles/CommunalStyle";
 import styled from "styled-components";
 
@@ -12,7 +12,7 @@ const CategoryInput = styled.input`
   font-family: "SUITLight";
   font-size: 17px;
   &:focus {
-    outline: none;
+    outline: 1px solid #3fc87e;
   }
   text-align: center;
 `;
@@ -28,7 +28,7 @@ const PriceInput = styled.input`
   font-family: "SUITLight";
   font-size: 17px;
   &:focus {
-    outline: none;
+    outline: 1px solid #3fc87e;
   }
   padding-left: 28px;
 `;
@@ -41,16 +41,68 @@ const Unit = styled.span`
   top: 20px;
 `;
 
-function CategoryAmountComponent({ data }) {
+function CategoryAmountComponent({
+  data,
+  isUpdating,
+  amount,
+  setAmount,
+  setIsMinimumCategory,
+}) {
+  const [category, setCategory] = useState(data.category);
+  const [price, setPrice] = useState(convertStringNum(data.amount));
+  function removeBtnClick() {
+    if (amount.length > 2) {
+      setAmount((prev) => prev.filter((itm) => itm.category !== data.category));
+      setIsMinimumCategory(false);
+    } else {
+      setIsMinimumCategory(true);
+    }
+  }
+  function handleCategoryChange(e) {
+    setCategory(e.target.value);
+    setAmount((prev) =>
+      prev.map((itm) =>
+        itm.category === data.category
+          ? { ...itm, category: e.target.value }
+          : itm
+      )
+    );
+  }
+  function handlePriceChange(e) {
+    const { value } = e.target;
+    // value의 값이 숫자가 아닐경우 빈문자열로 replace 해버림.
+    const onlyNumber = value.replace(/[^0-9]/g, "");
+    const formattedNumber = new Intl.NumberFormat().format(onlyNumber);
+    setPrice(formattedNumber);
+    setAmount((prev) =>
+      prev.map((itm) =>
+        itm.category === data.category ? { ...itm, amount: onlyNumber } : itm
+      )
+    );
+  }
   return (
     <Horizontal style={{ marginTop: "14px" }}>
-      <CategoryInput readOnly value={data.category} />
+      <CategoryInput
+        readOnly={!isUpdating}
+        value={category}
+        onChange={handleCategoryChange}
+      />
       <div style={{ position: "relative" }}>
-        <PriceInput readOnly value={data.amount} />
+        <PriceInput
+          readOnly={!isUpdating}
+          value={price}
+          onChange={handlePriceChange}
+        />
         <Unit>원</Unit>
       </div>
+      {isUpdating === true && <button onClick={removeBtnClick}>빼기</button>}
     </Horizontal>
   );
 }
 
 export default CategoryAmountComponent;
+
+function convertStringNum(onlyNumber) {
+  const formattedNumber = new Intl.NumberFormat().format(onlyNumber);
+  return formattedNumber;
+}
