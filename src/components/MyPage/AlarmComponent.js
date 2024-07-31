eff401af89163f8e1f2f876f51f8dd48dee3ee56
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-07-31 16:46:02 +0900

    #48 feat: 알림 신청 취소 API 연결

--- src/components/MyPage/AlarmComponent.js
+++ src/components/MyPage/AlarmComponent.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRecoilValue } from "recoil";
+import { tokenState } from "../../store/atom";
 import styled from "styled-components";
 import AlarmImg from "../../imgs/alarmCheck.png";
 import {
@@ -82,10 +85,24 @@ const SubmitButton = styled.button`
   margin-top: 60px;
   margin-left: 170px;
 `;
+const CancleButton = styled.button`
+  font-family: "SUITMedium";
+  font-size: 14px;
+  width: 100px;
+  height: 40px;
+  border: none;
+  border-radius: 20px;
+  color: white;
+  background-color: #d3d3d3;
+  cursor: pointer;
+  margin-top: 60px;
+  margin-left: 170px;
+`;
 
 function AlarmComponent() {
+  const userToken = useRecoilValue(tokenState);
   //TODO: API 불러와서 알람톡 신청 받고있는 사람인지 파악 후 setAlarm 초기값 세팅
-  const [alarm, setAlarm] = useState(false);
+  const [alarm, setAlarm] = useState(true);
   const [consent, setConsent] = useState(false);
   const [checks, setChecks] = useState({
     all: false,
@@ -115,6 +132,27 @@ function AlarmComponent() {
     console.log("handleAlarmClick");
     setConsent(!consent);
   };
+  const onClickConsent = () => {
+    //TODO: 사용자 이름이랑 전화번호 넘겨줘서 알림 신청하게 하는 API 연결
+    console.log("알람 신청");
+  };
+  const onClickAlarmCancle = () => {
+    console.log("알람 취소");
+    axios
+      .delete(`${process.env.REACT_APP_BASE_URL}/user/alarm-message/no`, {
+        headers: {
+          Authorization: "Bearer " + userToken,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("알람 설정 취소 response", response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Wrapper>
       <SSOBBIBox>
@@ -134,51 +172,86 @@ function AlarmComponent() {
         )}
       </SSOBBIBox>
       {consent ? (
-        <></>
-      ) : (
         <>
-          <NoCenterVertical>
-            <BoxTitle>약관 동의</BoxTitle>
-            <hr
-              style={{
-                border: "0.5px solid #D9D9D9",
-                margin: "0px 32px 20px 32px",
-              }}
-            />
-            <NoCenterHorizontal style={{ marginBottom: "15px" }}>
-              <CheckBox
-                checked={checks.all}
-                onClick={() => handleCheckBoxClick("all")}
-              />{" "}
-              모두 동의하기
-            </NoCenterHorizontal>
-            <NoCenterHorizontal style={{ marginBottom: "15px" }}>
-              <CheckBox
-                checked={checks.terms1}
-                onClick={() => handleCheckBoxClick("terms1")}
-              />{" "}
-              (필수) 이용약관
-            </NoCenterHorizontal>
-            <NoCenterHorizontal style={{ marginBottom: "15px" }}>
-              <CheckBox
-                checked={checks.terms2}
-                onClick={() => handleCheckBoxClick("terms2")}
-              />{" "}
-              (필수) 이용약관
-            </NoCenterHorizontal>
-            <NoCenterHorizontal
-              style={{ marginBottom: "15px", width: "240px" }}
-            >
-              <CheckBox
-                checked={checks.marketing}
-                onClick={() => handleCheckBoxClick("marketing")}
-              />{" "}
-              (선택) 마케팅 활용 및 공급사 상품 및 배송을 위한 개인정보 제 3자
-              제공 동의
-            </NoCenterHorizontal>
-            <SubmitButton disabled={!allRequiredChecked}>제출하기</SubmitButton>
-          </NoCenterVertical>
+          {alarm ? (
+            <>
+              {" "}
+              <NoCenterVertical>
+                <BoxTitle>약관 동의</BoxTitle>
+                <hr
+                  style={{
+                    border: "0.5px solid #D9D9D9",
+                    margin: "0px 32px 20px 32px",
+                  }}
+                />
+                <NoCenterHorizontal style={{ marginBottom: "15px" }}>
+                  <CheckBox
+                    checked={checks.all}
+                    onClick={() => handleCheckBoxClick("all")}
+                  />{" "}
+                  모두 동의하기
+                </NoCenterHorizontal>
+                <NoCenterHorizontal style={{ marginBottom: "15px" }}>
+                  <CheckBox
+                    checked={checks.terms1}
+                    onClick={() => handleCheckBoxClick("terms1")}
+                  />{" "}
+                  (필수) 이용약관
+                </NoCenterHorizontal>
+                <NoCenterHorizontal style={{ marginBottom: "15px" }}>
+                  <CheckBox
+                    checked={checks.terms2}
+                    onClick={() => handleCheckBoxClick("terms2")}
+                  />{" "}
+                  (필수) 이용약관
+                </NoCenterHorizontal>
+                <NoCenterHorizontal
+                  style={{ marginBottom: "15px", width: "240px" }}
+                >
+                  <CheckBox
+                    checked={checks.marketing}
+                    onClick={() => handleCheckBoxClick("marketing")}
+                  />{" "}
+                  (선택) 마케팅 활용 및 공급사 상품 및 배송을 위한 개인정보 제
+                  3자 제공 동의
+                </NoCenterHorizontal>
+                <SubmitButton
+                  disabled={!allRequiredChecked}
+                  onClick={onClickConsent}
+                >
+                  제출하기
+                </SubmitButton>
+              </NoCenterVertical>
+            </>
+          ) : (
+            <>
+              <NoCenterVertical>
+                <BoxTitle>알림톡 취소</BoxTitle>
+                <hr
+                  style={{
+                    border: "0.5px solid #D9D9D9",
+                    margin: "0px 32px 20px 32px",
+                  }}
+                />
+                <NoCenterHorizontal
+                  style={{
+                    marginBottom: "15px",
+                    width: "240px",
+                    marginLeft: "32px",
+                  }}
+                >
+                  알림톡을 더 이상 받지 않으실 경우 아래 취소하기 버튼을
+                  눌러주세요.
+                </NoCenterHorizontal>
+                <CancleButton onClick={onClickAlarmCancle}>
+                  취소하기
+                </CancleButton>
+              </NoCenterVertical>
+            </>
+          )}
         </>
+      ) : (
+        <></>
       )}
     </Wrapper>
   );
