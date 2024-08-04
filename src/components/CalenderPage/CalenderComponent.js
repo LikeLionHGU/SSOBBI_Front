82558e2b1f7f30d391bed2ae234ce3715dcdbef4
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-08-04 19:05:13 +0900

    style: 달력 label 이동 막기

--- src/components/CalenderPage/CalenderComponent.js
+++ src/components/CalenderPage/CalenderComponent.js
@@ -102,7 +102,7 @@ function CalenderComponent({ onMonthChange, setApiMonth }) {
           handleActiveStartDateChange({ ...e, action: e.action })
         }
         formatDay={(locale, date) => moment(date).format("D")} // 일 제거 숫자만 보이게
-        formatYear={(locale, date) => moment(date).format("YYYY")} // 네비게이션 눌렀을때 숫자 년도만 보이게
+        formatYear={(locale, date) => moment(date).format("YY")} // 네비게이션 눌렀을때 숫자 년도만 보이게
         formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
         calendarType="gregory" // 일요일 부터 시작
         showNeighboringMonth={false} // 전달, 다음달 날짜 숨기기
