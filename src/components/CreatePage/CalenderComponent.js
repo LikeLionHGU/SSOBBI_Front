1a05a0c7619d15dd9c42d9ce11400e191155a21a
sungyu0309 <tjsrbyt@gmail.com>
2024-08-06 22:06:26 +0900

    style : 캘린더 형식 변경

--- src/components/CreatePage/CalenderComponent.js
+++ src/components/CreatePage/CalenderComponent.js
@@ -81,7 +81,7 @@ function Calender({ setSelectDate }) {
         onChange={handleDateChange}
         formatDay={(locale, date) => moment(date).format("DD")} // 일 제거 숫자만 보이게
         formatYear={(locale, date) => moment(date).format("YYYY")} // 네비게이션 눌렀을때 숫자 년도만 보이게
-        formatMonthYear={(locale, date) => moment(date).format("MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
+        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
         calendarType="gregory" // 일요일 부터 시작
         next2Label={null} // +1년 & +10년 이동 버튼 숨기기
         prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
